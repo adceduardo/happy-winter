02e493e75e00255c99a4a208d9c9af380ef35e0d
EDUARDO ALVES DE CARVALHO <105119276+adceduardo@users.noreply.github.com>
2026-08-14 00:47:04 -0300

    [feat] new content structuring and style improvements.

--- happy-winter/src/components/Content/Content.jsx
+++ happy-winter/src/components/Content/Content.jsx
@@ -1,19 +1,24 @@
-import "./Content.css";
-import { FaHands } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
-import { GiWinterHat } from "react-icons/gi";
+import './Content.css';
+import { FaHands } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
+import { GiWinterHat } from 'react-icons/gi';
 
 export function Content() {
   return (
-    <section>
-      <article>
-        <h1>Happy Winter </h1>
-        <h2>Artigos de Inverno</h2>
-        <img src="logo.png"></img>
-        <p> Aqueça seus dias com estilo, conforto e muito carinho</p>
+    <section className="presentation-container">
+      <article className="presentation-main">
+        <div className="background-container"></div>
+        <div className="content">
+          <img src="logo.png"></img>
+          <div>
+            <h1>Happy Winter </h1>
+            <h2>Artigos de Inverno</h2>
+            <p> Aqueça seus dias com estilo, conforto e muito carinho</p>
+          </div>
+        </div>
       </article>
 
-      <article>
+      <article className="">
         <ul>
           <li>
             <FaHands />
