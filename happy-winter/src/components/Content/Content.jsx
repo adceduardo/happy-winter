1db4d23973ab377750b8f643d972ab2557edba44
eduardo <eduhsbs@gmail.com>
2026-08-14 12:04:04 -0300

    [feat] styling and structuring of presentation content

--- happy-winter/src/components/Content/Content.jsx
+++ happy-winter/src/components/Content/Content.jsx
@@ -1,24 +1,29 @@
-import './Content.css';
-import { FaHands } from 'react-icons/fa';
-import { FaHeart } from 'react-icons/fa';
-import { GiWinterHat } from 'react-icons/gi';
+import "./Content.css";
+import { FaHands } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { GiWinterHat } from "react-icons/gi";
 
 export function Content() {
   return (
-    <section className="presentation-container">
+    <section className="presentation-section">
       <article className="presentation-main">
         <div className="background-container"></div>
         <div className="content">
-          <img src="logo.png"></img>
-          <div>
-            <h1>Happy Winter </h1>
-            <h2>Artigos de Inverno</h2>
-            <p> Aqueça seus dias com estilo, conforto e muito carinho</p>
+          <div className="content-information">
+            
+            <img src="logo.png"></img>
+            <div className="content-information-body">
+              <h1>Happy Winter </h1>
+              <div>
+                <h2>Artigos de Inverno</h2>
+                <p> Aqueça seus dias com estilo, conforto e muito carinho</p>
+              </div>
+            </div>
           </div>
         </div>
       </article>
 
-      <article className="">
+      <article className="presentation-footer">
         <ul>
           <li>
             <FaHands />
